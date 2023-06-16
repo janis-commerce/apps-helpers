3841ad95fb90b22510ee8555e73f94e2913dd808
Mauricio Fazzano <mauricio.fazzano@janis.im>
2023-06-16 16:11:25 -0300

    refactor position util

--- tests/isValidFormatPosition.js
+++ tests/isValidFormatPosition.js
@@ -3,7 +3,7 @@ import isValidFormatPosition from '../lib/isValidFormatPosition.js';
 
 describe('isValidFormatPosition helper', () => {
 	it('return false when argument is not a valid position', () => {
-		[3, NaN, [], { test: 1 }, null, undefined, { positionKey: [], positionId: 123 }].forEach(
+		[3, NaN, [], { test: 1 }, null, undefined, { positionId: 12 }, { positionKey: 12 }].forEach(
 			(arg) => assert.equal(isValidFormatPosition(arg), false)
 		);
 	});
@@ -15,7 +15,7 @@ describe('isValidFormatPosition helper', () => {
 
 		const { positionId, positionKey } = validPosition;
 
-		[(validPosition, { positionId }, { positionKey }, { positionKey: 123 })].forEach((arg) =>
+		[validPosition, { positionId }, { positionKey }].forEach((arg) =>
 			assert.equal(isValidFormatPosition(arg), true)
 		);
 	});
