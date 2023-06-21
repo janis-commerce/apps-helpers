1a931ea41d4a3aa6692ddc45c4e97ed1a9bdba70
Fernando Colom <fernando_colom@janis.im>
2023-06-21 12:20:24 -0300

    add env variable

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -1,4 +1,5 @@
-echo "$GITHUB_REF_NAME" 
+export TAG=$(echo $GITHUB_REF_NAME | sed -e 's/v//')
+echo $TAG
 awk -v ver="$GITHUB_REF_NAME" '
 	/^#+ \[/ { if (p) { exit }; if ($2 == "["ver"]") { p=1; next} } p
 	' CHANGELOG.md | sed '${/^[[:space:]]*$/d;}' > RELEASE_NOTES.md
