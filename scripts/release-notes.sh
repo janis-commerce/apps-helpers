9db7c849c08112cf428f766abfdba764676dbc3c
Fernando Colom <fernando_colom@janis.im>
2023-06-21 12:13:14 -0300

    test variable form github

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -1,3 +1,4 @@
+echo "$GITHUB_REF_NAME" 
 awk -v ver="$GITHUB_REF_NAME" '
 	/^#+ \[/ { if (p) { exit }; if ($2 == "["ver"]") { p=1; next} } p
 	' CHANGELOG.md | sed '${/^[[:space:]]*$/d;}' > RELEASE_NOTES.md
