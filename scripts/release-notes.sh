1172636a0a75c908fa0b5bbc9c03c79263b8be19
Fernando Colom <fernando_colom@janis.im>
2023-06-21 12:24:35 -0300

    test changes

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -1,6 +1,6 @@
 export TAG=$(echo $GITHUB_REF_NAME | sed -e 's/v//')
-echo $TAG
-awk -v ver="$GITHUB_REF_NAME" '
+
+awk -v ver="$TAG" '
 	/^#+ \[/ { if (p) { exit }; if ($2 == "["ver"]") { p=1; next} } p
 	' CHANGELOG.md | sed '${/^[[:space:]]*$/d;}' > RELEASE_NOTES.md
 
